29134f93e980e13d58d23dfd61ccc65abbea0fab
OLC <2565128043@qq.com>
2019-12-31 06:58:20 +0800

    Revert "格式化代码&改了下XmlApi可能引发NRE的代码"

--- me.cqp.ritsukage.RitsukageBot/Native.Csharp/App/LuaEnv/TcpServer.cs
+++ me.cqp.ritsukage.RitsukageBot/Native.Csharp/App/LuaEnv/TcpServer.cs
@@ -1,53 +1,69 @@
 ﻿using System;
-using System.Collections.Concurrent;
+using System.Collections;
 using System.Text;
 using System.Threading.Tasks;
 using Native.Csharp.Sdk.Cqp.Enum;
 using SimpleTCP;
 
-namespace Native.Csharp.App.LuaEnv {
-    internal class TcpServer {
+namespace Native.Csharp.App.LuaEnv
+{
+    class TcpServer
+    {
         //需要发送的包列表
-        private static readonly ConcurrentQueue<string> toSend = new ConcurrentQueue<string>();
-
+        private static ArrayList toSend = new ArrayList();
         //每个包发送间隔时间（可以自己改）
-        private const int packTime = 1000;
+        private static int packTime = 1000;
 
-        private static readonly SimpleTcpServer server = new SimpleTcpServer();
-
-        public static void Start() {
-            try {
+        private static SimpleTcpServer server = new SimpleTcpServer();
+        public static void Start()
+        {
+            try
+            {
                 server.StringEncoder = Encoding.UTF8;
                 server.Start(23333);
-                Common.CqApi.AddLoger(LogerLevel.Info, "tcp server", "tcp server started!");
+                Common.CqApi.AddLoger(LogerLevel.Info, "tcp server",
+                    "tcp server started!");
                 server.DataReceived += (sender, msg) => {
-                    LuaEnv.RunLua($"message=[[{msg.MessageString.Replace("]", "] ")}]] ", "envent/ReceiveTcp.lua");
+                    LuaEnv.RunLua(
+                        $"message=[[{msg.MessageString.Replace("]", "] ")}]] ",
+                        "envent/ReceiveTcp.lua");
                 };
-            } catch (Exception e) {
-                Common.CqApi.AddLoger(LogerLevel.Error, "tcp server", "tcp server start failed!\r\n" + e);
+            }
+            catch(Exception e)
+            {
+                Common.CqApi.AddLoger(LogerLevel.Error, "tcp server", 
+                    "tcp server start failed!\r\n"+e);
             }
 
             //消息发送队列
-            Task.Run(
-                () => {
-                    while (true) {
-                        while (toSend.TryDequeue(out string dataResult)) {
-                            server.Broadcast(dataResult);
-                            Task.Delay(packTime).Wait();
-                        }
-                        Task.Delay(200).Wait(); //等等，防止卡死
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    while (toSend.Count > 0)
+                    {
+                        string temp = toSend[0].ToString();//取出第一个数据
+                        toSend.RemoveAt(0);
+                        server.Broadcast(temp);
+                        Task.Delay(packTime).Wait();
                     }
+                    Task.Delay(200).Wait();//等等，防止卡死
                 }
-            );
+            });
         }
 
         [LuaAPIFunction("apiTcpSend")]
-        public static void Send(string msg) {
-            try {
+        public static void Send(string msg)
+        {
+            try
+            {
                 //server.Broadcast(msg);
-                toSend.Enqueue(msg);
-            } catch (Exception e) {
-                Common.CqApi.AddLoger(LogerLevel.Error, "tcp server", "tcp server send failed!\r\n" + e);
+                toSend.Add(msg);
+            }
+            catch (Exception e)
+            {
+                Common.CqApi.AddLoger(LogerLevel.Error, "tcp server",
+                    "tcp server send failed!\r\n" + e);
             }
         }
     }
